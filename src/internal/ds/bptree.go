2486d71f5cc084d745988c0d0486bc6569f66b6d
JasonAlbertNewton <j2004nol@gmail.com>
2024-02-04 15:50:20 +0800

    rebuild the bp tree structure

--- src/internal/ds/bptree.go
+++ src/internal/ds/bptree.go
@@ -3,3 +3,26 @@
 package main
 
 //rewrite whole bptree concept
+
+//parent class for internal page and leaf page
+//Resources: https://15445.courses.cs.cmu.edu/spring2023/project2/#b+tree-structure-2a
+
+type BPTreePage struct{ 
+    page_type uint32 //either internal or leaf
+    size_ uint32
+    max_size_ uint32
+}
+
+type BPTreeInternalPage struct{
+    keys []int
+    pages []*BPTreePage
+}
+
+type BPTreeLeafPage struct{
+    keys []int
+    //use interger as value for testing purpose/ replace this section with the data once finish this section.
+    value []byte
+}
+
+//insertation deletion 
+func InsertNode(id int , root *BPTreeInternalPage)(){}
